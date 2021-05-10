9651213d3927966c99e5ac383168faef335b69f0
Luis Valgoi <luisvalgoi@gmail.com>
2021-05-09 23:38:37 -0300

    feat: dinamicity into the lists and values

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,15 +1,17 @@
 import React from "react";
-import List from "../List";
 
+import List from "../List";
 import { Container } from "./styles";
+
+import { loadLists } from "../../services/api";
+const lists = loadLists();
 
 export default function Board() {
   return (
     <Container>
-      <List title="Tarefas" />
-      <List title="Tarefas" />
-      <List title="Tarefas" />
-      <List title="Tarefas" />
+      {lists.map((list) => (
+        <List key={list.title} data={list} />
+      ))}
     </Container>
   );
 }
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,14 +2,16 @@ import React from "react";
 
 import { Container, Label } from "./styles";
 
-export default function Card() {
+export default function Card({ data }) {
   return (
     <Container>
       <header>
-        <Label color="#7149c1" />
+        {data.labels.map((label) => (
+          <Label key={label} color={label} />
+        ))}
       </header>
-      <p>Fazer Migração completa</p>
-      <img src="https://yt3.ggpht.com/yti/ANoDKi4qCG_uRovC-iVq0XSsyYeKp4xa9uoHUypejAgg4F4=s88-c-k-c0x00ffffff-no-rj-mo" alt="avatar" />
+      <p>{data.content}</p>
+      {data.user && <img src={data.user} alt="" />}
     </Container>
   );
 }
