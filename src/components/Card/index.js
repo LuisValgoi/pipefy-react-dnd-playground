76c95874785e74e8f3ed7b230663d355a91c1aed
Luis Valgoi <luisvalgoi@gmail.com>
2021-05-09 23:24:36 -0300

    feat: added card and its style

---
+++ src/components/Card/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Container, Label } from "./styles";
+
+export default function Card() {
+  return (
+    <Container>
+      <header>
+        <Label color="#7149c1" />
+      </header>
+      <p>Fazer Migração completa</p>
+      <img src="https://yt3.ggpht.com/yti/ANoDKi4qCG_uRovC-iVq0XSsyYeKp4xa9uoHUypejAgg4F4=s88-c-k-c0x00ffffff-no-rj-mo" alt="avatar" />
+    </Container>
+  );
+}
